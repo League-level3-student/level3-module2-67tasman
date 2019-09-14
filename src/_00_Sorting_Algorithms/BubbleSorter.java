dd48b662e567235183d503e69c4adb429b7f265c
League Student <league@iMac-25.attlocal.net>
2019-09-14 15:59:04 -0700

    sortedarraychecker, swapping digits, bubblesorter, heapsorter

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -3,6 +3,7 @@ package _00_Sorting_Algorithms;
 public class BubbleSorter extends Sorter{
 	public BubbleSorter() {
 		type = "Bubble";
+	
 	}
 	
 	//1. Use the bubble sorting algorithm to sort the array.
@@ -10,7 +11,23 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		boolean b = false;
+		while(b==false) {
+			b = true;
+		for (int i = 0; i < array.length - 1; i++) {
+			
+			if (array[i + 1] < array[i]) {
+				int k = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = k;
+				b = false;
+				display.updateDisplay();
+				
+			} else {
+
+			}
+		}
+		}
 	}
 	
 }
--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -1,41 +1,73 @@
 package _00_Sorting_Algorithms;
 
 public class _00_SortedArrayChecker {
-	//1. Write a static method called intArraySorted. 
-	//   This method takes in an array of integers
-	//   and it returns a boolean.
-	//   The method returns true if the integer
-	//   array is in ascending order and false otherwise
-static boolean intArraySorted(int[] a) {
-	boolean b=false;
-	for(int i=0; i<a.length-1; i++)
-	if(a[i+1]>a[i]) {
-		b = true;
+	// 1. Write a static method called intArraySorted.
+	// This method takes in an array of integers
+	// and it returns a boolean.
+	// The method returns true if the integer
+	// array is in ascending order and false otherwise
+	static boolean intArraySorted(int[] a) {
+		boolean b = true;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i + 1] < a[i]) {
+				b = false;
+			}
+		}
+		return b;
+
 	}
-	return b;
-	
-}
-	
-	//2. Write a static method called doubleArraySorted. 
-	//   This method takes in an array of doubles
-	//   and it returns a boolean.
-	//   The method returns true if the double
-	//   array is in ascending order and false otherwise
 
-	
-	//3. Write a static method called charArraySorted. 
-	//   This method takes in an array of characters
-	//   and it returns a boolean.
-	//   The method returns true if the character
-	//   array is in alphabetical order and false otherwise
-	//   (You can compare characters just like integers)
+	// 2. Write a static method called doubleArraySorted.
+	// This method takes in an array of doubles
+	// and it returns a boolean.
+	// The method returns true if the double
+	// array is in ascending order and false otherwise
 
-	
-	//4. Write a static method called stringArraySorted. 
-	//   This method takes in an array of Strings
-	//   and it returns a boolean.
-	//   The method returns true if the String
-	//   array is in alphabetical order and false otherwise
-	//   (Use the compareTo(String) method)
+	static boolean doubleArraySorted(double[] a) {
+		boolean b = true;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i + 1] < a[i]) {
+				b = false;
+			}
+		}
+		return b;
+
+	}
+	// 3. Write a static method called charArraySorted.
+	// This method takes in an array of characters
+	// and it returns a boolean.
+	// The method returns true if the character
+	// array is in alphabetical order and false otherwise
+	// (You can compare characters just like integers)
+
+	static boolean charArraySorted(char[] a) {
+		boolean b = true;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i + 1] < a[i]) {
+				b = false;
+
+			}
+		}
+		return b;
+
+	}
+
+	// 4. Write a static method called stringArraySorted.
+	// This method takes in an array of Strings
+	// and it returns a boolean.
+	// The method returns true if the String
+	// array is in alphabetical order and false otherwise
+	// (Use the compareTo(String) method)
+	static boolean stringArraySorted(String[] a) {
+		boolean b = true;
+		for (int i = 0; i < a.length - 1; i++) {
+
+			if (a[i + 1].compareTo(a[i]) < 0) {
+				b = false;
+			}
+		}
+		return b;
+
+	}
 
 }
