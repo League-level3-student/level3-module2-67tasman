1f17e5fe101d29ae5e13b5a704cc82b4f0d76c3b
League Student <league@iMac-25.attlocal.net>
2019-09-21 15:59:34 -0700

    selection, merge, insertion

--- src/_00_Sorting_Algorithms/InsertionSorter.java
+++ src/_00_Sorting_Algorithms/InsertionSorter.java
@@ -12,11 +12,21 @@ public class InsertionSorter extends Sorter {
 	void sort(int[] array, SortingVisualizer display) {		
 		//1. make a for loop that starts at 1 and goes through 
 		// the length of the array 
-
+for (int i = 1; i < array.length; i++) {
 			//2. make another for loop that starts at i and counts down
         	//   while j is greater than 0
-
+	for (int j = i; j >0; j--) {
 				//3. if the element at j is less than the element at j - 1,
             	//   then swap them
+		int j2 = j-1;
+		if(array[j]<array[j2]) {
+			int s = array[j];
+			array[j]=array[j2];
+			array[j2]=s;
+		}
+		display.updateDisplay();
 	}
+	
+}	
+}
 }
